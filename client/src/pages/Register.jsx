e0261730b56e7b33a8d93b0649012c6e464f9121
Ashutosh <ashutoshdeepu256@gmail.com>
2026-07-04 14:17:01 +0530

    Complete frontend authentication module

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../services/api";
+
 function Register() {
-  return <h1>📝 Register Page</h1>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "student",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = async () => {
+    try {
+      const { data } = await api.post("/users/register", formData);
+
+      toast.success(data.message);
+
+      navigate("/login");
+
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Registration Failed"
+      );
+    }
+  };
+
+  return (
+    <div style={{ width: "350px", margin: "80px auto" }}>
+      <h2>Register</h2>
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleChange}
+        style={{ width: "100%", padding: "10px", marginBottom: "10px" }}
+      />
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+        style={{ width: "100%", padding: "10px", marginBottom: "10px" }}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+        style={{ width: "100%", padding: "10px", marginBottom: "10px" }}
+      />
+
+      <select
+        name="role"
+        value={formData.role}
+        onChange={handleChange}
+        style={{ width: "100%", padding: "10px", marginBottom: "15px" }}
+      >
+        <option value="student">Student</option>
+        <option value="recruiter">Recruiter</option>
+      </select>
+
+      <button
+        onClick={handleRegister}
+        style={{
+          width: "100%",
+          padding: "10px",
+          background: "#2563eb",
+          color: "white",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        Register
+      </button>
+    </div>
+  );
 }
 
 export default Register;
